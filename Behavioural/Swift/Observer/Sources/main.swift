f5a429760799aec2384f05289c7772e40458133d
André Felipe <academicworks.adf@gmail.com>
2024-11-10 17:47:00 -0300

    fixing a mistake

--- Behavioural/Swift/Observer/Sources/main.swift
+++ Behavioural/Swift/Observer/Sources/main.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-let string: String = "Hello, world!"
+let string: String = "Hello, world!!!!!"
 var string2: String = ""
 
 for i in 0...5 {
