27aec14d1685584330aeea6688c28cdf62eecb75
André Felipe <academicworks.adf@gmail.com>
2025-02-15 10:24:49 -0300

    feat: implement Builder pattern for smartphones and cars in Dart

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 import 'Creational/Dart/FactoryMethod/example 1/main.dart';
 import 'Creational/Dart/FactoryMethod/example 2/main.dart';
-import 'Creational/Dart/Builder/example 1/main.dart';
+import 'Creational/Dart/Builder/main.dart';
 import 'Creational/Dart/AbstractFactory/main.dart';
 import 'Creational/Dart/Singleton/main.dart';
 import 'Structural/Dart/Adapter/main.dart';
